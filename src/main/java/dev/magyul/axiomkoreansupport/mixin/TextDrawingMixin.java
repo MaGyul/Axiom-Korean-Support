b1ead0f7d04cb3a335d886511c5263520e87b509
Ma_Gyul <49198230+MaGyul@users.noreply.github.com>
2024-07-09 16:01:10 +0900

    Downgrade Java version (21 -> 17)

--- src/main/java/dev/magyul/axiomkoreansupport/mixin/TextDrawingMixin.java
+++ src/main/java/dev/magyul/axiomkoreansupport/mixin/TextDrawingMixin.java
@@ -1,15 +1,19 @@
 package dev.magyul.axiomkoreansupport.mixin;
 
 import com.moulberry.axiom.tools.text.TextDrawing;
+import net.minecraft.resource.Resource;
+import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.Optional;
+
 @Mixin(TextDrawing.class)
 public class TextDrawingMixin {
-    @Redirect(method = "loadFont", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;of(Ljava/lang/String;Ljava/lang/String;)Lnet/minecraft/util/Identifier;"))
-    private static Identifier loadFont(String namespace, String path) {
-        return Identifier.of(namespace, "pretendard-medium.ttf");
+    @Redirect(method = "loadFont", at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ResourceManager;getResource(Lnet/minecraft/util/Identifier;)Ljava/util/Optional;"))
+    private static Optional<Resource> loadFont(ResourceManager manager, Identifier identifier) {
+        return manager.getResource(identifier.withPath("pretendard-medium.ttf"));
     }
 }
